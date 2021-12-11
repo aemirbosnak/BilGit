ea730826ed56c93a7666f697b50d313dfa2a51e4
Atika Zeynep Evmez <zeynep.evmez@ug.bilkent.edu.tr>
2021-12-11 12:40:20 +0300

    Home fragment updated, color palette implemented (old color palette still remains as a comment)

--- app/src/main/java/com/example/trybil/view/HomeFragment.java
+++ app/src/main/java/com/example/trybil/view/HomeFragment.java
@@ -66,6 +66,19 @@ public class HomeFragment extends Fragment {
             }
         });
 
+        homeFragmentBinding.cardView4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.placeFragment);
+            }
+        });
+
+        homeFragmentBinding.cardView5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                navController.navigate(R.id.placeFragment);
+            }
+        });
     }
 
 }
