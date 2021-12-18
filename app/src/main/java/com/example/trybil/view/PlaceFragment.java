9d64d4823da0da7df73598d9e65c5dd858e3b32b
Atika Zeynep Evmez <zeynep.evmez@ug.bilkent.edu.tr>
2021-12-18 17:24:20 +0300

    Updates on placeFragment

--- app/src/main/java/com/example/trybil/view/PlaceFragment.java
+++ app/src/main/java/com/example/trybil/view/PlaceFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -14,6 +15,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.RatingBar;
 
+import com.example.trybil.databinding.HomeFragmentBinding;
 import com.example.trybil.databinding.PlaceFragmentBinding;
 import com.example.trybil.databinding.ProfileFragmentBinding;
 import com.example.trybil.viewmodel.MainViewModel;
@@ -22,22 +24,38 @@ import com.example.trybil.viewmodel.MainViewModel;
 public class PlaceFragment extends Fragment {
     private PlaceFragmentBinding placeFragmentBinding;
     private MainViewModel mViewModel;
-    Button button;
-    RatingBar ratingStars;
-    int myRating = 0;
-    //I'm not sure about these values
-    int totalRate = 0;
-    int rateNum = 0;
+    CardView cardView;
+    HomeFragmentBinding homeFragmentBinding;
+    RatingBar ratingBar;
+    RatingBar averageRate;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mViewModel = new ViewModelProvider(getActivity()).get(MainViewModel.class);
+        ratingBar = placeFragmentBinding.ratingBar;
+        averageRate = placeFragmentBinding.averageRate;
+        cardView = homeFragmentBinding.cardView1;
 
         mViewModel.getPlace().observe(this, new Observer<String>() {
             @Override
             public void onChanged(String s) {
-                placeFragmentBinding.textView.setText(s);
+                placeFragmentBinding.placeName.setText(s);
+                placeFragmentBinding.averageRate.setRating();
+            }
+        });
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                float newRate = Float.parseFloat(mViewModel.getPlace().getValue()) + rating;
+                mViewModel.getPlace().observe(getViewLifecycleOwner(), new Observer<String>() {
+                    @Override
+                    public void onChanged(String s) {
+
+                        placeFragmentBinding.placeName.setText(s);
+                    }
+                });
             }
         });
     }
@@ -52,39 +70,5 @@ public class PlaceFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        /*
-        placeFragmentBinding.ratingBar.setOnRatingBarChangeListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
-
-         */
-        /*
-        private ProfileFragmentBinding profileFragmentBinding;
-        private MainViewModel mainViewModel;
-
-        @Override
-        public void onCreate(@Nullable Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            mainViewModel = new ViewModelProvider(this).get(MainViewModel.class);
-
-        }
-
-        @Override
-        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                @Nullable Bundle savedInstanceState) {
-            profileFragmentBinding = ProfileFragmentBinding.inflate(inflater, container, false);
-            return profileFragmentBinding.getRoot();
-        }
-
-        @Override
-        public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-            super.onViewCreated(view, savedInstanceState);
-        }
-
-         */
     }
 }
