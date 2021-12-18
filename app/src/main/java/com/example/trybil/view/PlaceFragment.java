b8d15c5453c678395782077e0e56bd7d42b9fe72
Emir Bosnak <emir.bosnak@ug.bilkent.edu.tr>
2021-12-18 18:45:47 +0300

    rating bar commented out hotfix

--- app/src/main/java/com/example/trybil/view/PlaceFragment.java
+++ app/src/main/java/com/example/trybil/view/PlaceFragment.java
@@ -1,52 +1,47 @@
 package com.example.trybil.view;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.RatingBar;
-
 import com.example.trybil.databinding.HomeFragmentBinding;
 import com.example.trybil.databinding.PlaceFragmentBinding;
-import com.example.trybil.databinding.ProfileFragmentBinding;
 import com.example.trybil.viewmodel.MainViewModel;
 
 
 public class PlaceFragment extends Fragment {
     private PlaceFragmentBinding placeFragmentBinding;
     private MainViewModel mViewModel;
-    CardView cardView;
     HomeFragmentBinding homeFragmentBinding;
-    RatingBar ratingBar;
-    RatingBar averageRate;
+    //CardView cardView;
+    //RatingBar ratingBar;
+    //RatingBar averageRate;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mViewModel = new ViewModelProvider(getActivity()).get(MainViewModel.class);
-        ratingBar = placeFragmentBinding.ratingBar;
-        averageRate = placeFragmentBinding.averageRate;
-
-        cardView = homeFragmentBinding.cardView1;
+        //ratingBar = placeFragmentBinding.ratingBar;
+        //averageRate = placeFragmentBinding.averageRate;
+        //cardView = homeFragmentBinding.cardView1;
 
         mViewModel.getPlace().observe(this, new Observer<String>() {
             @Override
             public void onChanged(String s) {
                 placeFragmentBinding.placeName.setText(s);
-                placeFragmentBinding.averageRate.setRating();
+                placeFragmentBinding.averageRate.setRating(1);
             }
         });
 
-        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+        /*
+        placeFragmentBinding.ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
             @Override
             public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
                 float newRate = Float.parseFloat(mViewModel.getPlace().getValue()) + rating;
@@ -58,7 +53,7 @@ public class PlaceFragment extends Fragment {
                     }
                 });
             }
-        });
+        });*/
     }
 
     @Override
