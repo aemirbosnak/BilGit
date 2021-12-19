352da3e283a3ef855528b9481e24ee6bbc4b40b3
Arda <arda.iynem@ug.bilkent.edu.tr>
2021-12-19 20:24:16 +0300

    *Removed Scroll View From Profile Fragment since it already contains RecyclerView

--- app/src/main/java/com/example/trybil/view/ProfileFragment.java
+++ app/src/main/java/com/example/trybil/view/ProfileFragment.java
@@ -91,9 +91,6 @@ public class ProfileFragment extends Fragment {
                         }
                     }
                 });
-
-        //update the height of ScrollView widget according to number of friends
-        profileFragmentBinding.getRoot().setMinimumHeight(700 + 200 * userFriend.size());
     }
 
     @Override
